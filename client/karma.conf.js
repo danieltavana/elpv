b157c166da2191abf9407b2303fccd34f7918d0f
radproject <radhouanef@gmail.com>
2015-02-11 21:10:45 +0100

    angular / gulp barebones

---
+++ client/karma.conf.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function(config) {
+
+  config.set({
+    autoWatch : false,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+        'karma-chrome-launcher',
+        'karma-jasmine'
+    ]
+  });
+};
