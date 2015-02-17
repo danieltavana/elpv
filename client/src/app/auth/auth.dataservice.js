551b8690d01c93c3565feb73d9006b91e515d6a0
radproject <radhouanef@gmail.com>
2015-02-17 23:19:18 +0100

    configuring routers

---
+++ client/src/app/auth/auth.dataservice.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('pv.auth',[])
+  .factory('Auth',function(){
+    return{
+      hello:'rad'
+    };
+  });
--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,18 +1,25 @@
 'use strict';
 
-angular.module('client', ['ngAnimate', 'ngRoute', 'ui.bootstrap','client.pv'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/newpv', {
-        templateUrl: 'app/pv/newpv.html',
-        controller: 'PvController'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-;
+angular.module('client', ['ngAnimate', 'ui.router', 'ui.bootstrap','client.pv'])
+  .config(function ($stateProvider,$urlRouterProvider) {
+
+    $stateProvider
+
+              .state('home', {
+                  url: '/home',
+                  templateUrl: 'app/main/main.html',
+                  controller: 'MainCtrl',
+                  data: {
+                      access: 0
+                  }
+              })
+              .state('newpv', {
+                  url: '/newpv',
+                  templateUrl: 'app/pv/newpv.html',
+                  controller: 'PvController',
+                  data: {
+                      access: 0
+                  }
+              });
+    $urlRouterProvider.otherwise('/home');
+  });
